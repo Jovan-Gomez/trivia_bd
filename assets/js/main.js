032331eb3c116f05d87c80cfecdccde827f95557
Jovan-Gomez <jovanjacobgomezcarrillo@gmail.com>
2021-04-23 13:00:21 -0500

    Primera Version de Trivia

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+'use strict'
+document.querySelector('#choose_yes').addEventListener('click', ()=>{location.href='./select-trivia.html'});/* Abre el formulario para seleccionar tipo de trivia*/
+document.querySelector('#choose_random').addEventListener('click', ()=>{location.href='./trivia.html'});/* Abre trivia aleatoria*/
